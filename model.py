c557f92f4d3ac00388aaf6ae915799ce2763c382
Rohan Shah <rohan_2710@yahoo.co.in>
2019-10-15 22:28:25 +1100

    Create model.py

---
+++ model.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class KittiDetector(nn.Module):
+    def __init__(self):
+        super(KittiDetector, self).__init__()
+        self.
